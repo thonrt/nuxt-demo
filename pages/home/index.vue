763a848e44aec0bf46d0f09088f3ec343230855b
bduan <bella.duan@morningstar.com>
2020-12-02 17:04:25 +0800

    push v tag

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">Demo</h1>
+        <h1 class="logo-font">Nuxt Demo</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
