32e09d9557a01fbe4d9ca2e6fbba78cce253dc4c
bduan <bella.duan@morningstar.com>
2020-12-03 11:34:16 +0800

    publish v0.1.5 tag

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">Nuxt Demo</h1>
+        <h1 class="logo-font">Nuxt Test Demo</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
